91e9667f59538c0b993b401a668ca13cb9f463e7
mateus-henrique-silva <henrique.s@gsuite.com.br>
2022-09-20 15:45:11 -0300

    Realizando polimento no site da Work

---
+++ site_work/.history/src/Scripts/redirect/scripts_20220920153738.js
@@ -0,0 +1,11 @@
+function onClickLink(){
+    window.location.href=" https://api.whatsapp.com/send?phone=5522999422728&text=ol%C3%A1%2C%20tudo%20bem%20%3F"
+}
+
+function facebook(){
+
+}
+
+function instagramk(){
+    
+}
---
+++ site_work/.history/src/Scripts/redirect/scripts_20220920153858.js
@@ -0,0 +1,12 @@
+function onClickLink() {
+  window.location.href =
+    " https://api.whatsapp.com/send?phone=5522999422728&text=ol%C3%A1%2C%20tudo%20bem%20%3F";
+}
+
+function facebook() {
+  window.location.href = "https://www.instagram.com/workbandalarga/?next=%2F";
+}
+
+function instagramk() {
+    window.location.href = "https://www.instagram.com/workbandalarga/?next=%2F";
+}
